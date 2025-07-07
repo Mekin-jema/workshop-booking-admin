b94f3c820483eb1ebdbee9c888424df07fd0c278
Mekin Jemal <mekinjemal999@gmail.com>
2025-07-07 15:43:28 +0300

    refactor: clean up Vite configuration by removing unused input options

--- vite.config.ts
+++ vite.config.ts
@@ -9,12 +9,16 @@ export default defineConfig({
     react()
   ],
   build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        // ...
-        // List all files you want in your build
-      }
+        rollupOptions: {
+            output:{
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
     }
-  }
+
 })
+
