d04f08dcc29cc644c58864da8df7301c72351130
Mekin Jemal <mekinjemal999@gmail.com>
2025-07-07 15:30:32 +0300

    refactor: remove unused chunkSplitPlugin from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-// vite.config.ts
-import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
-
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    chunkSplitPlugin(),
-        tailwindcss()
-        ,react()],
+    tailwindcss(),
+    react()
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        // ...
+        // List all files you want in your build
+      }
+    }
+  }
 })
-
-
-
